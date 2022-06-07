8dcf0cdba6777076aaae6f7faaffc9b021304783
Lhary <hilaryokems@gmail.com>
2022-06-07 21:21:58 +0100

    second commit

--- src/Review.js
+++ src/Review.js
@@ -11,13 +11,13 @@ const Review = () => {
             <h2 className='text-center'>Customer's Review</h2>
 
             <div className='row mt-5 pt-0 pt-lg-5'>
-                <div className='col-lg position-relative'>
+                <div className='col-lg position-relative text-center text-lg-start'>
                     <img src={img7} className='img-fluid' alt="" />
                     <div className=' review-text1'>
-                        <img src={img8} className='img-fluid ms-3' alt="" />
+                        <img src={img8} className='img-fluid ms-3 review-quote' alt="" />
                         <p className='text-center w-75 ms-5 text-white'>Trading on TradExpress mobile & web platforms has been a smooth  experience for me quite easy to navigate.</p>
                     </div>
-                    <div className='d-inline-flex ms-4 mt-2'>
+                    <div className='d-inline-flex ms-4 mt-2 review-pics'>
                         <img src={lhary} className='review-img' alt="" />
                         <div className='ms-2'>
                             <p><b>Ireti</b> <br />Nigeria</p>
@@ -25,28 +25,27 @@ const Review = () => {
                         </div>
                     </div>
                 </div>
-                <div className='col-lg position-relative mt-5 mt-lg-0'>
+                <div className='col-lg position-relative text-center text-lg-start mt-5 mt-lg-0 review-img2'>
                     <img src={img7} className='img-fluid' alt="" />
                     <div className=' review-text2'>
-                        <img src={img8} className='img-fluid ms-3' alt="" />
+                        <img src={img8} className='img-fluid ms-3 review-quote' alt="" />
                         <p className='text-center w-75 ms-5 text-white'>They have the best rate compared to other platforms with fast payout.</p>
                     </div>
-                    <div className='d-inline-flex ms-4 mt-2'>
+                    <div className='d-inline-flex ms-4 mt-2 review-pics review-pics2'>
                         <img src={lhary} className='review-img' alt="" />
                         <div className='ms-2'>
-                            <p><b>Bayo</b> <br />USA</p>
-                            
+                            <p><b>Bayo</b> <br />USA</p>    
                         </div>
                     </div>
 
                 </div>
-                <div className='col-lg position-relative mt-5 mt-lg-0'>
+                <div className='col-lg position-relative text-center text-lg-start mt-5 pt-2 pt-lg-0 mt-lg-0'>
                     <img src={img7} className='img-fluid' alt="" />
                     <div className=' review-text3'>
-                        <img src={img8} className='img-fluid ms-3' alt="" />
+                        <img src={img8} className='img-fluid ms-3 review-quote' alt="" />
                         <p className='text-center w-75 ms-5 text-white'>Easy to fund and withdraw coins or cash on their platform, I will definitely trade with them again.</p>
                     </div>
-                    <div className='d-inline-flex ms-4 mt-2'>
+                    <div className='d-inline-flex ms-4 mt-2 review-pics'>
                         <img src={lhary} className='review-img' alt="" />
                         <div className='ms-2'>
                             <p><b>Olagoke</b> <br />Nigeria</p>
